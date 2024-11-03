dfe1158961d0bff2cbbe9fb7af6cff9a1fb000aa
Regallion <Agent-Dima@yandex.ru>
2024-11-03 10:41:28 +0300

    compatibility

--- src/ui/sections/tasks.vue
+++ src/ui/sections/tasks.vue
@@ -29,6 +29,27 @@ export default {
 	collapseSection() {
 
 	},
+	methods: {
+		groupMap(maparr) {
+			let groupedMap = new Map
+			let groupHolder = {}
+			for (let a of maparr) {
+				let b = a.group ? a.group : "other"
+				if (Array.isArray(groupHolder[b]))
+				 {
+					groupHolder[b].push(a)
+				 }
+				 else groupHolder[b] = Array.of(a)
+				
+			}
+			for (let c in groupHolder) {
+				groupedMap.set(c, groupHolder[c])
+			}
+			
+			return groupedMap
+		}
+
+	},
 	computed: {
 
 		hall() { return Profile.hall; },
@@ -39,16 +60,24 @@ export default {
 
 		},
 		visibleUpgrades() {
-			return Game.state.upgrades.filter(upgrade =>  !this.locked(upgrade) && this.show(upgrade) && (!upgrade.owned || upgrade.repeat)).concat(this.mergedtasks.filter(task => !this.locked(task) && !task.morality && this.show(task) && (task.max || !task.repeat)));
+			return Game.state.upgrades.filter(upgrade => !this.locked(upgrade) && this.show(upgrade) && (!upgrade.owned || upgrade.repeat)).concat(this.mergedtasks.filter(task => !this.locked(task) && !task.morality && this.show(task) && (task.max || !task.repeat)));
 		},
 
 		groupedTasks() {
-			let ret = Map.groupBy(this.visibleTasks, (task) => task.group ? task.group : "other");
-			return ret;
+			//Compatibility change
+			//let ret = Map.groupBy(this.visibleTasks, (task) => task.group ? task.group : "other");
+			//return ret; 
+			let oof = this.groupMap(this.visibleTasks)
+			return oof
+			
 		},
 		groupedUpgrades() {
-			let ret = Map.groupBy(this.visibleUpgrades, (upgrade) => upgrade.group ? upgrade.group : "other");
-			return ret;
+			//Compatibility change
+			//let ret = Map.groupBy(this.visibleUpgrades, (upgrade) => upgrade.group ? upgrade.group : "other");
+			//return ret;
+			let oof = this.groupMap(this.visibleUpgrades)
+			return oof
+			
 		},
 		classes() {
 			return Game.state.classes.filter(v => !this.locked(v) && this.show(v));
@@ -66,11 +95,11 @@ export default {
 	<div class="main-tasks" ref="hidables">
 		<div class="config"><button ref="btnHides" class="btnConfig"></button></div>
 		<div class="div-hs">Tasks</div>
-		<TaskGroup v-for="group in Array.from(groupedTasks.keys()).toSorted()" :title="group">
+		<TaskGroup v-for="group in Array.from(groupedTasks.keys()).sort()" :title="group">
 			<upgrades class="task-list" :items="groupedTasks.get(group)" :preventClick="inConfig" />
 		</TaskGroup>
 		<div v-if="groupedUpgrades.length != 0" class="div-hs">Upgrades</div>
-		<TaskGroup v-for="group in Array.from(groupedUpgrades.keys()).toSorted()" :title="group">
+		<TaskGroup v-for="group in Array.from(groupedUpgrades.keys()).sort()" :title="group">
 			<upgrades class="task-list" :items="groupedUpgrades.get(group)" :preventClick="inConfig" />
 		</TaskGroup>
 		<div v-if="classes.length != 0" class="div-hs">Classes</div>
